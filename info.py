47651a13851a40c9e993f2e96818fd16c8c30ff3
Divytech <155970643+Divytech@users.noreply.github.com>
2024-05-25 21:53:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,7 +34,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 # MongoDB information
 SECONDDB_URI = environ.get('SECONDDB_URI', "mongodb+srv://dqthedonar74:dqthedonar74@cluster0.hyysf85.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://devurl:devurl@cluster0.aekafsv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
 FILE_DB_NAME = environ.get("FILE_DB_NAME", DATABASE_NAME)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
