0fea40919a377493e258ace0cf0f600ef2f558c6
Divytech <155970643+Divytech@users.noreply.github.com>
2024-05-26 14:26:19 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -250,7 +250,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.edit_message_media(InputMediaPhoto(random.choice(PICS), script.HELP_TXT.format(query.from_user.mention), enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons))     
         
     elif query.data == "about":
-        await query.send_message(chat_id=chat_id, text='Hello from your bot!')
+        await query.send_message(script.HELP_TXT.format(query.from_user.mention), enums.ParseMode.HTML))
         
     elif query.data == "source":
         buttons = [[
