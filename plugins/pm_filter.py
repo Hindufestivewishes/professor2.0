ad44b7e9b1e6faa6598f605b4b7b95f99f3304ec
Divytech <155970643+Divytech@users.noreply.github.com>
2024-06-17 10:00:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -98,7 +98,7 @@ async def pm_spoll_tester(bot, query):
         k = (movie, files, offset, total_results)
         await pm_AutoFilter(bot, query, k)
     else:
-        k = await query.message.edit('This Movie Not Found In Database. If this movie has been released recently then please try after some time. If it is old movie then I'm definitely sure you can find your movie on our 2nd bot check out now - <a href=https://t.me/dev_autof_bot>iPapkornFbot</a>')
+        k = await query.message.edit('This Movie Not Found In Database. If this movie has been released recently then please try after some time. If it is old movie then I'm definitely sure you can find your movie on our 2nd bot check out now - @Dev_autof_bot')
         await asyncio.sleep(30)
         await k.delete()
 
